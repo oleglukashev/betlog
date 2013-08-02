71523b554544046681d4bbc30799d0d266552818
Oleg <incognitorus@gmail.com>
2013-08-02 22:25:59 +0400

    add rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betlog::Application.config.secret_token = '44edb5575894bb415cecaa242670ff7288740a22a4aa7d2b857d8abec85ab82de136518470267803cd5581c5a988bf8de1aaa3347fa63a924286403b91090421'
