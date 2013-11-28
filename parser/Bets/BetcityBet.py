876e7d22f2497d0ab31e0b6bfa66eb4990d9b430
Oleg Lukashev <incognitorus@gmail.com>
2013-11-28 23:46:35 +0400

    правка сбора инфы с бетсити

--- parser/Bets/BetcityBet.py
+++ parser/Bets/BetcityBet.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 import logger
 import datetime
+import time
 import yaml
 from datetime import date
+import random
 
 import sqlalchemy
 from sqlalchemy import *
@@ -13,258 +15,160 @@ from sqlalchemy.ext.declarative import declarative_base
 import urllib3
 import lxml.html
 import lxml.cssselect
+from xml.dom.minidom import parseString
 from Modules.Dictionary import Dictionary
-from Modules import UsedChampionships
 
 
 class BetcityBet():
   
   def __init__( self ):
-    self.link = self.setConfigData()
+    self.bookmaker = "Betcity"
     self.teams_not_found = {}
     self.current_sport = ""
     self.current_country = ""
     self.current_championship = ""
+    self.result = {}
 
-  def setConfigData( self ):
-    bookmakers = yaml.load( open('parser/config/bookmakers.yml', 'r') )
-    for key, value in bookmakers.items():
-      if key == 'betcity':
-        return value
+  def getChampionshipsDataFromConfig( self ):
+    file = open( 'parser/Bets/config/links.xml', 'r' )
+    file_read = file.read()
+    file.close()
+    data = parseString( file_read )
+    return data.getElementsByTagName('championship')
 
-  def getContentFromUrl( self ):
+
+
+  def getContentByUrl( self, url ):
     http = urllib3.PoolManager()
-    cookies = {'Cookie': self.link['header'] }
-    page = http.request( 'GET', self.link['url'], headers=cookies )
-    doc = lxml.html.document_fromstring(page.data)
-    return doc
+    page = http.request( 'POST', url, { 'line_id[]': url[47:] } )
+
+    result = False
+    if ( page.status == 200 ):
+      return lxml.html.document_fromstring(page.data)
+    else:
+      print("-- error: url " + url + " not access")
 
 
   def parse( self ):
-    content = self.getContentFromUrl()
-    print("--- Content has loaded ---")
-
-    result = {}
     i = 0
+    championships = self.getChampionshipsDataFromConfig()
+    
+    for championship in championships:
+      for bookmaker in championship.getElementsByTagName('bookmaker'):
+        if bookmaker.getAttribute("value") == self.bookmaker:
+          for link in bookmaker.getElementsByTagName("link"):
 
-    for events_block in content.cssselect('body > table'):
-      if ( len( events_block.cssselect('tbody#line') ) > 0 ):
-        event_hash = {}
+            championship_content = None
+            
+            if ( len( link.getAttribute("value") ) > 0 ):
+              championship_content = self.getContentByUrl( link.getAttribute("value") )
+            else:
+              print("-- warning: no url for championship: " + championship.getAttribute("value") )
 
-        title_str = events_block.cssselect('thead tr td b')[0].text_content().strip(" \r\n")
+            if ( championship_content is not None ):
+              if ( len( championship_content.cssselect("tbody#line") ) ):
+                self.current_sport = championship.parentNode.parentNode.parentNode.getAttribute("value")
+                self.current_country = championship.parentNode.getAttribute("value")
+                self.current_championship = championship.getAttribute("value")
+                
+                event_hash = {
+                  "bookmaker": self.bookmaker,
+                  "sport": self.current_sport,
+                  "country": self.current_country,
+                  "championship": self.current_championship,
+                  "events_data": self.getTeamsAndCoefficientsFromEventDom( championship_content )
+                }
 
-        if ( self.skipOrNotTitleByWord( title_str ) == False ):
-          sport = self.getSportFromTitle( title_str )
-          country = self.getCountryFromTitle( title_str ) if self.getCountryFromTitle( title_str ) else "Международный"
-          championship = self.getChampionshipFromTitle( title_str )
-
-          file = open('display.txt', 'a', encoding='utf-8')
-
-          if ( UsedChampionships.findSport( sport ) is not None ):
-            if ( UsedChampionships.findCountryBySport( country, sport ) is not None ):
-              if ( UsedChampionships.findChampionshipBySportAndCountry( championship, sport, country ) is not None ):
-                event_hash['bookmaker'] = "Betcity"
-                self.current_championship = event_hash['championship'] = UsedChampionships.findChampionshipBySportAndCountry( championship, sport, country )
-                self.current_country = event_hash['country'] = UsedChampionships.findCountryBySport( country, sport )
-                self.current_sport = event_hash['sport'] = UsedChampionships.findSport( sport )
-                event_hash['teams_and_coefficients'] = self.getEventCoefficientFromDom( events_block )
-
-                result[i] = event_hash
+                self.result[i] = event_hash
 
                 i += 1
+
               else:
-                file.write( title_str + " - не найден Чемпионат\r\n")
-            else:
-              file.write( title_str + " - не найдена Страна\r\n")
-          else:
-            file.write( title_str + " - не найдне Спорт\r\n")
- 
-          file.close()
+                print("-- data on this page not founded --")
 
     return result
 
 
-  def getEventCoefficientFromDom( self, events_dom ):
+  def getTeamsAndCoefficientsFromEventDom( self, championship_content ):
     result = {}
     i = 0
     date_event = ""
     date = ""
+    head_tbody = None
 
-    for tbody in events_dom.cssselect('tbody'):
+    for tbody in championship_content.cssselect('tbody'):
+      event_hash = {}
+
+      if ( tbody.get("class") == "chead" ):
+        head_tbody = tbody
+
       if ( tbody.get("class") == "date" ):
         date = tbody.cssselect('tr td')[0].text.strip(" \r\n")
 
       if ( tbody.get("id") == "line"):
-        team1_td_position = self.getFirstTeamPosition( events_dom )
-        team1 = self.getDataFromHtmlByPosition( team1_td_position, tbody )
-        '''if ( self.getTeam( team1 ) is not None ):
-          event_hash['first_team'] = self.getTeam( team1 )
-        else:
-          self.showTeamNotFound( team1 )'''
 
-        team2_td_position = self.getSecondTeamPosition( events_dom )
-        team2 = self.getDataFromHtmlByPosition( team2_td_position, tbody )
-        '''if ( self.getTeam( team2 ) is not None ):
-          event_hash['first_team'] = self.getTeam( team2 )
-        else:
-          self.showTeamNotFound( team2 )'''
+        count = 0
+        for td in tbody.cssselect("tr[class] > td"):
+          if ( count <= len( head_tbody.cssselect("tr > td") ) - 1 ):
 
-        print(team1)
-        print(team2)
+            key = self.getKeyByHeadTitle( head_tbody.cssselect("tr > td")[count].text_content().strip(" \r\n"), None )
+            if ( key in event_hash ):
+              key = self.getKeyByHeadTitle( head_tbody.cssselect("tr > td")[count].text_content().strip(" \r\n"), 'skip_first' )
+            
+            event_hash[key] = td.text_content().strip(" \r\n")
 
-        '''date_event = date + " " + tbody.cssselect("tr:first-child td")[0].text.strip(" \r\n")
-        event_hash['date_event'] = self.getDate( date_event )
+            count += 1
 
-        first_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'first' )
-        event_hash['first'] = self.getCoefficientFromHtmlByPosition( event_item, first_position ) if first_position is not None else None
-
-        draw_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'draw' )
-        event_hash['draw'] = self.getCoefficientFromHtmlByPosition( event_item, draw_position ) if draw_position is not None else None
-
-        second_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'second' )
-        event_hash['second'] = self.getCoefficientFromHtmlByPosition( event_item, second_position ) if second_position is not None else None
-
-        first_or_draw_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'first_or_draw' )
-        event_hash['first_or_draw'] = self.getCoefficientFromHtmlByPosition( event_item, first_or_draw_position ) if first_or_draw_position is not None else None
-
-        first_or_second_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'first_or_second' )
-        event_hash['first_or_second'] = self.getCoefficientFromHtmlByPosition( event_item, first_or_second_position ) if first_or_second_position is not None else None
-
-        draw_or_second_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'draw_or_second' )
-        event_hash['draw_or_second'] = self.getCoefficientFromHtmlByPosition( event_item, draw_or_second_position ) if draw_or_second_position is not None else None
-
-        first_fora_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'first_fora' )
-        event_hash['first_fora'] = self.getCoefficientFromHtmlByPosition( event_item, first_fora_position ) if first_fora_position is not None else None
-
-        second_fora_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'second_fora' )
-        event_hash['second_fora'] = self.getCoefficientFromHtmlByPosition( event_item, second_fora_position ) if second_fora_position is not None else None
-
-        total_less_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'total_less' )
-        event_hash['total_less'] = self.getCoefficientFromHtmlByPosition( event_item, total_less_position ) if total_less_position is not None else None
-
-        total_more_position = self.getPositionFromHtmlByCoefficientType( event_dom, 'total_more' )
-        event_hash['total_more'] = self.getCoefficientFromHtmlByPosition( event_item, total_more_position ) if total_more_position is not None else None
-
-        result[i] = event_hash
-        i += 1'''
-
+            result[i] = event_hash
+            i += 1
+    print(result)   
     return result
 
 
 
-  def getFirstTeamPosition( self, events_dom ):
-    variations_of_title = { 'Команда 1', 'Спортсмен 1', 'Игрок 1' }
-    return self.getTeamPositionByTitle( variations_of_title, events_dom )
+  def getKeyByHeadTitle( self, title, skip_first ):
+    title_asscociation = {
+      'время' : ['date_event'],
+      'команда 1' : ['team1'],
+      'игрок 1' : ['team1'],
+      'спортсмен 1' : ['team1'],
+      'команда 2' : ['team2'],
+      'игрок 2' : ['team2'],
+      'спортсмен 2' : ['team2'],
+      '1': ['first'],
+      'X': ['draw'],
+      '2': ['second'],
+      '1X': ['first_or_draw'],
+      'X2': ['draw_or_second'],
+      'кф': ['first_fora', 'second_fora'],
+      'фора': ['coeff_first_fora', 'coeff_second_fora'],
+      'тотал': ['total'],
+      'мен': ['total_less_position'],
+      'бол': ['total_more_position']
+    }
+
+    for key, value in title_asscociation.items():
+      if key == title:
+        return title_asscociation[key][1 if skip_first is not None else 0]
 
 
 
-  def getSecondTeamPosition( self, events_dom ):
-    variations_of_title = { 'Команда 2', 'Спортсмен 2', 'Игрок 2' }
-    return self.getTeamPositionByTitle( variations_of_title, events_dom )
+  def showTeamNotFound( self, not_found_team_str ):
+    print("------ team not found -------")
+    print("sport: " + self.current_sport )
+    print("country: " + self.current_country )
+    print("championship: " + self.current_championship)
+    print("team: " + not_found_team_str + "\r\n")
 
 
-
-  def getTeamPositionByTitle( self, variations_of_title, events_dom ):
-    count = 0
-    first_chead = events_dom.cssselect('tbody.chead')[0]
-    for td in first_chead.cssselect('td'):
-      for item_variation in variations_of_title:
-        if td.text.strip(" \r\n") == item_variation:
-          return count
-
-      count += 1
-
-
-  def getDataFromHtmlByPosition( self, position, event_block ):
-    count = 0;
-
-    for td in event_block.cssselect("tr td"):
-      if position == count:
-        return td.cssselect("b")[0].text.strip(" \r\n")
-
-      count += 1
-    
-
-  def getTeamByTitleFromHtml( self, title_str, events_block ):
-    for tbody in events_block.cssselect("tbody"):
-      if ( tbody.get("class") == "chead"):
-        for td, key in tbody.cssselect("th td"):
-          print(key)
-
-
-  def getSportFromTitle( self, title ):
-    title_array = title.split(". ")
-
-    for title_element in title_array:
-      if Dictionary.findSport( title_element ):
-        return Dictionary.findSport( title_element )
-
-  def getChampionshipFromTitle( self, title ):
-    title_array = title.split(". ")
-
-    for title_element in title_array:
-      if Dictionary.findChampionship( title_element ):
-        return Dictionary.findChampionship( title_element )
-
-  def getCountryFromTitle( self, title ):
-    title_array = title.split(". ")
-
-    for title_element in title_array:
-      if Dictionary.findCountry( title_element ):
-        return Dictionary.findCountry( title_element )
 
   def getTeam( self, team ):
     if Dictionary.findTeam( team ):
       return Dictionary.findTeam( team )
 
-  def skipOrNotTitleByWord( self, title ):
-    ignore_words = ['Итоги', 'Женщины']
 
-    for item in ignore_words:
-      if ( title.find( item ) != -1 ):
-        return True
-
-    return False
 
   def getDate( self, str_date ):
     return datetime.datetime.strptime( str_date, "%d.%m.%Y %H:%M" )
 
-  def getPositionFromHtmlByCoefficientType( self, container, coefficient_type ):
-    coefficients_type_association = {
-      '1' : 'first',
-      'X' : 'draw',
-      '2' : 'second',
-      '1X' : 'first_or_draw',
-      '12' : 'first_or_second',
-      'X2' : 'draw_or_second',
-      'Фора1' : 'first_fora',
-      'Фора2' : 'second_fora',
-      'Тотал мен.' : 'total_less',
-      'Тотал бол.' : 'total_more',
-    }
 
-
-    tr_dom = container.cssselect('table.foot-market > tr')[0]
-
-    count = 0
-    for th_dom in tr_dom.cssselect('th.coupone'):
-      a_dom = th_dom.cssselect('a')
-
-      if ( len( a_dom ) ):
-        key = a_dom[0].text_content().strip(" \r\n")
-
-        if key in coefficients_type_association.keys():
-          if coefficients_type_association[ key ] == coefficient_type:
-            return count
-      count += 1
-
-
-  def getCoefficientFromHtmlByPosition( self, container, position ):
-    '''количество тн на 1 меньше чем тд'''
-    result = container.cssselect('tr.event-header > td')[position + 1]
-    result = result.cssselect('span')[0].text.strip(" \r\n")
-
-    if ( len(result) ):
-      return float(result)
-    else:
-      return None
