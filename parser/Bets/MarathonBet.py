f31c3584a013545eed8d53fa69c15f05d0cb8480
Oleg Lukashev <incognitorus@gmail.com>
2013-12-03 23:03:10 +0400

    правка марафона

--- parser/Bets/MarathonBet.py
+++ parser/Bets/MarathonBet.py
@@ -269,7 +269,7 @@ class MarathonBet():
 
 
   def showTeamNotFound( self, not_found_team_str ):
-    file = open( "display.txt", 'a', encoding='utf-8' )
+    file = open( "teams_not_found.txt", 'a', encoding='utf-8' )
     file.write("------ team not found -------\r\n")
     file.write("sport: " + self.current_sport + "\r\n" )
     file.write("country: " + self.current_country + "\r\n" )
