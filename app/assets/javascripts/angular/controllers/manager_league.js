3aad5be0992bc80be024fb2a1cbe417b69b28220
Oleg Lukashev <incognitorus@gmail.com>
2013-12-17 00:31:39 +0400

    добалвлены команды + начал блок менеджер лиг

---
+++ app/assets/javascripts/angular/controllers/manager_league.js
@@ -0,0 +1,12 @@
+betlog_controllers.controller('ManagerLeague', [
+  '$scope', 
+  '$rootScope',
+  '$http', 
+  'filterFilter', 
+  '$animate', 
+  '$route', 
+  '$routeParams',
+  'Storage',
+  function( $scope, $rootScope, $http, filterFilter, $animate, $route, $routeParams, Storage ) {
+
+  }])
