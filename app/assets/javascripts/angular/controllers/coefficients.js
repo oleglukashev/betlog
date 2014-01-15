de1767604384bf74107f967b92e4f91074bcb4d8
Oleg Lukashev <incognitorus@gmail.com>
2014-01-15 13:42:35 +0400

    правка карусели

--- app/assets/javascripts/angular/controllers/coefficients.js
+++ app/assets/javascripts/angular/controllers/coefficients.js
@@ -45,21 +45,6 @@ betlog_controllers.controller('Coefficients', [
       return result;
     }
 
-    $scope.getCoefficientsDatesEvent = function(coefficient_item) {
-      if ( coefficient_item ) {
-        var result = [];
-        var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': coefficient_item.event_id }, true );
-
-        coefficients.map(function(coefficient) {
-          if ( result.indexOf( coefficient.created_at ) === -1 ) {
-            result.push( $filter('date')(coefficient.created_at, "dd.MM") );
-          }
-        });
-
-        return result;
-      }
-    }
-
     $scope.getCoefficientsDatesEventOnlyOneBookmaker = function(coefficient_item) {
       if ( coefficient_item ) {
         var result = [];
@@ -75,29 +60,7 @@ betlog_controllers.controller('Coefficients', [
       }
     }
 
-    $scope.getMaxCoefficientByEventAndName = function(event, param) {
-      var result = [];
-      var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': event.id }, true );
-
-      return coefficients.sort(function(a, b) {
-        a = a[param];
-        b = b[param];
-
-        return b - a;
-      })[0];
-    }
-
-    $scope.getMaxCoefficientFirstByEvent = function(event) {
-      return $scope.getMaxCoefficientByEventAndName(event, 'first');
-    }
-
-    $scope.getMaxCoefficientDrawByEvent = function(event) {
-      return $scope.getMaxCoefficientByEventAndName(event, 'draw');
-    }
-
-    $scope.getMaxCoefficientSecondByEvent = function(event) {
-      return $scope.getMaxCoefficientByEventAndName(event, 'second');
-    } 
+    
 
     $scope.getCoefficientsParamEventOnlyOneBookmaker = function(coefficient_item, param) {
       if ( coefficient_item ) {
@@ -114,20 +77,6 @@ betlog_controllers.controller('Coefficients', [
       }
     }
 
-    $scope.getCoefficientsParamEvent = function(coefficient_item, param) {
-      if ( coefficient_item ) {
-        var result = [];
-        var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': coefficient_item.event_id }, true );
-
-        coefficients.map(function(coefficient) {
-          if ( result.indexOf( coefficient.created_at ) === -1 ) {
-            result.push( coefficient[param] );
-          }
-        });
-
-        return result;
-      }
-    }
 
 
 
--- app/assets/javascripts/angular/controllers/favorites/MainFavoriteEventsController.js
+++ app/assets/javascripts/angular/controllers/favorites/MainFavoriteEventsController.js
@@ -11,12 +11,20 @@ betlog_controllers.controller('MainFavoriteEvents', [
     $scope.favorite_countries_by_sport = [];
     $scope.favorite_championships_by_sport_and_country = [];
     
+    $scope.getCoefficients = function() {
+        return Storage.getCoefficients();
+    }
+
     $scope.getEvents = function() {
       return Storage.getEvents();
     }
 
     $scope.getChampionships = function() {
       return Storage.getChampionships();
+    }
+
+    $scope.getBookmakers = function() {
+      return Storage.getBookmakers();
     }
 
     $scope.getFavoriteEvents = function() {
@@ -100,6 +108,64 @@ betlog_controllers.controller('MainFavoriteEvents', [
       return result;
     }
 
+    $scope.getMaxCoefficientByEventAndName = function(event, param) {
+      var result = [];
+      var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': event.id }, true );
+
+      return coefficients.sort(function(a, b) {
+        a = a[param];
+        b = b[param];
+
+        return b - a;
+      })[0];
+    }
+
+    $scope.getMaxCoefficientFirstByEvent = function(event) {
+      return $scope.getMaxCoefficientByEventAndName(event, 'first');
+    }
+
+    $scope.getMaxCoefficientDrawByEvent = function(event) {
+      return $scope.getMaxCoefficientByEventAndName(event, 'draw');
+    }
+
+    $scope.getMaxCoefficientSecondByEvent = function(event) {
+      return $scope.getMaxCoefficientByEventAndName(event, 'second');
+    }
+
+    $scope.getCoefficientsParamEvent = function(coefficient_item, param) {
+      if ( coefficient_item ) {
+        var result = [];
+        var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': coefficient_item.event_id }, true );
+
+        coefficients.map(function(coefficient) {
+          if ( result.indexOf( coefficient.created_at ) === -1 ) {
+            result.push( coefficient[param] );
+          }
+        });
+
+        return result;
+      }
+    }
+
+    $scope.getCoefficientsDatesEvent = function(coefficient_item) {
+      if ( coefficient_item ) {
+        var result = [];
+        var coefficients = filterFilter( $scope.getCoefficients(), { 'event_id': coefficient_item.event_id }, true );
+
+        coefficients.map(function(coefficient) {
+          if ( result.indexOf( coefficient.created_at ) === -1 ) {
+            result.push( $filter('date')(coefficient.created_at, "dd.MM") );
+          }
+        });
+
+        return result;
+      }
+    }
+
+    $scope.getBookmaker = function(id) {
+      return filterFilter( Storage.getBookmakers(), { 'id': id }, true)[0];
+    }
+
     $scope.reloadJcarousel = function() {
       $('.jcarousel').jcarousel();
     }
@@ -127,4 +193,9 @@ betlog_controllers.controller('MainFavoriteEvents', [
       Storage.getEvents();
     });
 
+    $rootScope.$on('reloadCoefficients', function() {
+        $scope.getCoefficients();
+    });
+
+    
   }])
